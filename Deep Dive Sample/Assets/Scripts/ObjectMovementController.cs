4f579c2a03c443ba3db919ef420e76465519d1e4
Jelmerrr <Jelljel@hotmail.com>
2023-11-07 13:28:08 +0100

    Le Turtel has arrived

--- Deep Dive Sample/Assets/Scripts/ObjectMovementController.cs
+++ Deep Dive Sample/Assets/Scripts/ObjectMovementController.cs
@@ -2,32 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Animation : MonoBehaviour
+public class ObjectMovementController : MonoBehaviour
 {
-    public GameObject soundEmitter;
+    public GameObject targetObject;
     Vector3 originalPosition;
-    Vector3 openPosition = new Vector3(0, 0 ,-15);
+    public Vector3 openPosition = new Vector3(0, 0 ,-15);
+    public float animationDuration = 10f;
 
     public bool isOpen = false;
     public bool inAnimation = false;
 
-    float openDuration = 10f;
     float currentTime = 0;
     float normalizedValue;
 
     void Start()
     {             
-        originalPosition = soundEmitter.transform.position;          //Store the starting position of the sidebar for return sequence
+        originalPosition = targetObject.transform.position;          //Store the starting position of the sidebar for return sequence
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.LeftControl) && inAnimation == false)  //Detects tab placement + failsafe no animations are running
         {
-            if (isOpen == false)                                     //Looks if the sidebar is opened or closed
+            if (isOpen == false)                                    //Looks if the sidebar is opened or closed
             {
                 currentTime = 0;                                    //Reset animation timer
-                //textHandler.TextUpdate();                           //Updates text with proper information
                 IEnumerator openCoroutine = PanelOpen();            //Index coroutine containing the opening animation
                 StartCoroutine(openCoroutine);                      //Starts the coroutine
                 isOpen = true;                                      //Updates status of sidebar
@@ -44,24 +43,24 @@ public class Animation : MonoBehaviour
         {
             if(isOpen == false)
             {
-                currentTime = openDuration;
+                currentTime = animationDuration;
             }
             else if(isOpen == true)
             {
-                currentTime = openDuration;
+                currentTime = animationDuration;
             }
         }
     }
 
     IEnumerator PanelOpen()                                         //Animation for opening the sidebar
     {
-        while (currentTime <= openDuration)                         //While the time spent in animation is smaller than the duration
+        while (currentTime <= animationDuration)                    //While the time spent in animation is smaller than the duration
         {
             inAnimation = true;                                     //Update animation status
             currentTime += Time.deltaTime;                          //Update time
-            normalizedValue = currentTime / openDuration;           //Value that is used to calculate speed and progression in animation
+            normalizedValue = currentTime / animationDuration;           //Value that is used to calculate speed and progression in animation
             normalizedValue = Mathf.Sin(normalizedValue * Mathf.PI * 0.5f); //Use a sin wave in order to create a fade out effect
-            soundEmitter.transform.position = Vector3.Lerp(originalPosition, openPosition, normalizedValue); //Use lerp to move object
+            targetObject.transform.position = Vector3.Lerp(originalPosition, openPosition, normalizedValue); //Use lerp to move object
             yield return null;                                      //Repeat coroutine
         }
         inAnimation = false;                                        //If animation is finished update status
@@ -69,13 +68,13 @@ public class Animation : MonoBehaviour
 
     IEnumerator PanelClose()                                        //Animation for closing the sidebar
     {
-        while (currentTime <= openDuration)                         //While the time spent in animation is smaller than the duration
+        while (currentTime <= animationDuration)                    //While the time spent in animation is smaller than the duration
         {
             inAnimation = true;                                     //Update animation status
             currentTime += Time.deltaTime;                          //Update time
-            normalizedValue = currentTime / openDuration;           //Value that is used to calculate speed and progression in animation
+            normalizedValue = currentTime / animationDuration;      //Value that is used to calculate speed and progression in animation
             normalizedValue = Mathf.Sin(normalizedValue * Mathf.PI * 0.5f); //Use a sin wave in order to create a fade out effect
-            soundEmitter.transform.position = Vector3.Lerp(openPosition, originalPosition, normalizedValue); //Use lerp to move object
+            targetObject.transform.position = Vector3.Lerp(openPosition, originalPosition, normalizedValue); //Use lerp to move object
             yield return null;                                      //Repeat coroutine
         }
         inAnimation = false;                                        //If animation is finished update status
