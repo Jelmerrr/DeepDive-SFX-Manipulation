571564ad609aa1b514325fb34c8b2f98fc8867e6
Jelmerrr <Jelljel@hotmail.com>
2023-11-28 09:48:27 +0100

    Revert Menu switching change

--- Deep Dive Sample/Assets/Scripts/CamSwitcher.cs
+++ Deep Dive Sample/Assets/Scripts/CamSwitcher.cs
@@ -7,21 +7,16 @@ public class CamSwitcher : MonoBehaviour
 {
     public Camera camMain;
     public Camera camSpecial;
-    public Camera camMenu;
 
     public Canvas soundGuide;
 
-    private Camera activeCam;
 
     void Start()
     {
         camMain.enabled = true;
         camSpecial.enabled = false;
-        camMenu.enabled = false;
 
         soundGuide.enabled = true;
-
-        activeCam = camMain;
     }
 
     void Update()
@@ -31,37 +26,6 @@ public class CamSwitcher : MonoBehaviour
             camMain.enabled = !camMain.enabled;
             camSpecial.enabled = !camSpecial.enabled;
             soundGuide.enabled = !soundGuide.enabled;
-
-            if(activeCam == camMain)
-            {
-                activeCam = camSpecial;
-            }
-            else if (activeCam == camSpecial)
-            {
-                activeCam = camMain;
-            }
-        }
-        if(Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (camMenu.enabled == false)
-            {
-                camMain.enabled = false;
-                camSpecial.enabled = false;
-                soundGuide.enabled = false;
-            }
-            if(camMenu.enabled == true)
-            {
-                if(activeCam == camMain)
-                {
-                    camMain.enabled = true;
-                    soundGuide.enabled = true;
-                }
-                if(activeCam == camSpecial)
-                {
-                    camSpecial.enabled = true;
-                }
-            }
-            camMenu.enabled = !camMenu.enabled;         
         }
     }
 }
